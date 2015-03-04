6bc9df0b548702291bc3084b81d4005526c97f4d
Hasse Bjork <hassebjork@users.noreply.github.com>
2015-03-04 20:48:40 +0100

    Widget headers in SVG. AJAX for refreshing headers every 10 minutes, except on mobile devices

--- weather-reader/www/index.php
+++ weather-reader/www/index.php
@@ -11,6 +11,9 @@ define( 'WINDGUST', 8 );
 define( 'WINDDIRECTION', 16 );
 define( 'RAINTOTAL', 32 );
 
+define( 'SVG_COLOR_BG', '80a2e0' );
+define( 'SVG_COLOR_WIND_ARROW', '2e8abb' );
+
 $mysqli = new mysqli( DB_HOST, DB_USER, DB_PASS, DB_DATABASE );
 $mysqli->set_charset( 'utf8' );
 
@@ -44,31 +47,20 @@ class Groups {
 			}
 			
 			if ( $has_data[$no] ) {
-				echo "\t"  . '<div id="graph' . $no . '" class="graph">'  
+				echo "\t"  . '<div id="graph' . $no . '" class="team">'  
 				. "\n\t\t" . '<div class="graph_title">' . "\n";
 				
 				foreach( $team as $id=>$sensor ) {
 					if ( $sensor->type & TEMPERATURE && $sensor->has_data() && count( $sensor->data ) > 8 ) {
-						echo "\t\t\t" . '<div id="temp' . $id . '" class="sens">' . "\n\t\t\t\t" 
-							. '<div class="s_name" style="color: #' . dechex( $sensor->color ) . '" title="' . $sensor->protocol . ':' . count( $sensor->data ) . '">' . $sensor->name . '</div>' . "\n\t\t\t\t"
-							. '<div class="t_cur" title="No. ' . count( $sensor->data ) . '">' . @$sensor->cur->temp . '&deg;C</div> ' . "\n\t\t\t\t"
-							. '<div class="s_cen">' 
-							. ( $sensor->battery == 0 ? '<div class="batt0">&nbsp;</div>' : '' )
-							. ( isset( $sensor->min->humid ) ? '<div class="humid">' 
-									. '<div class="h_max">' . $sensor->max->humid . '%</div>' 
-									. '<div class="h_cur">' . $sensor->cur->humid . '%</div>' 
-									. '<div class="h_min">' . $sensor->min->humid . '%</div></div>' : '' )
-							. '</div> ' . "\n\t\t\t\t"
-							. '<div class="t_max">' . @$sensor->max->temp . '&deg;C</div> ' . "\n\t\t\t\t"
-							. '<div class="t_min">' . @$sensor->min->temp . '&deg;C</div> ' . "\n\t\t\t"
-							. '</div>' . "\n";
+						echo $sensor->svg_head( "\t\t\t" );
+// 						echo $sensor->html_head( "\t\t\t" );
 					}
 				}
-				echo "\t\t</div>\n\t\t" . '<canvas id="graphCanvas' . $no .'" height="150" width="350"></canvas>' . "\n\t"
+				echo "\t\t</div>\n\t\t" . '<canvas id="graphCanvas' . $no .'" class="chart" height="150" width="350"></canvas>' . "\n\t"
 				.'</div>' . "\n\n";
 			}
 		}
-	
+// 		return;
 		echo "\n\t" 
 			. '<script>' . "\n\t\t"
 			. 'var lineOpts = {' . "\n\t\t\t"
@@ -226,7 +218,8 @@ class Sensor {
 	static function &from_array( &$row ) {
 		$sensor = new Sensor;
 		$sensor->id        = intval( $row['id'] );
-		$sensor->name      = str_replace( ' ', '&nbsp;', $row['name'] );
+		$sensor->name      = $row['name'];
+// 		$sensor->name      = str_replace( ' ', '&nbsp;', $row['name'] );
 		$sensor->protocol  = $row['protocol'];
 		$sensor->sensor_id = intval( $row['sensor_id'] );
 		$sensor->team      = intval( $row['team'] );
@@ -280,6 +273,46 @@ class Sensor {
 			}
 // 			var_dump( $row );
 		}
+	}
+
+	function svg_head( $tabs ) {
+// 	function svg_head( $tabs, $width, $height ) {
+		return $tabs
+ 			. '<svg id="svg_sensor' . $this->id .'" class="c_head" width="150px" height="56px" '
+// 			. '<svg id="tempSVG' . $this->id .'" class="c_head" width="' . $width .'" height="' . $height .'" '
+			. 'xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">' . "\n\t" . $tabs
+			. '<use xlink:href="#svg_head_bg" />' . "\n\t" . $tabs
+			. '<use  x="1%"  y="18" xlink:href="#svg_battery" style="opacity: .5; visibility: '. ( $this->battery == 0 ? 'visible' : 'hidden' ) . '" />' . "\n\t" . $tabs
+			. '<text x="49%" y="24" class="s_title" style="fill:#' . dechex( $this->color ) . '">' . $this->name .'</text>' . "\n\t" . $tabs
+			. '<g class="temp">' . "\n\t\t" . $tabs
+			. '<text x="49%" y="48" class="t_cur">' . $this->cur->temp .'&deg;C</text>' . "\n\t\t" . $tabs
+			. '<text x="98%" y="38" class="t_max">' . $this->max->temp .'&deg;C</text>' . "\n\t\t" . $tabs
+			. '<text x="98%" y="50" class="t_min">' . $this->min->temp .'&deg;C</text>' . "\n\t" . $tabs
+			. '</g>' . "\n\t" . $tabs
+			. '<g class="humid" style="visibility: '. ( isset( $this->min->humid ) ? 'visible' : 'hidden' ) . '">' . "\n\t\t" . $tabs
+			. '<text x="54%" y="36" class="h_max">' . $this->max->humid .' %</text>' . "\n\t\t" . $tabs
+			. '<text x="54%" y="44" class="h_cur">' . $this->cur->humid .' %</text>' . "\n\t\t" . $tabs
+			. '<text x="54%" y="52" class="h_min">' . $this->min->humid .' %</text>' . "\n\t" . $tabs
+			. '</g>' . "\n" . $tabs
+			. '</svg>' . "\n";
+	}
+
+	function html_head( $tabs ) {
+		return $tabs
+			. '<div id="temp' . $this->id . '" class="c_head">' . "\n\t" . $tabs
+			. '<div class="s_title" style="color: #' . dechex( $this->color ) . '" title="' . $this->protocol . ':' . count( $this->data ) . '">' . $this->name . '</div>' . "\n\t" . $tabs
+			. '<div class="t_cur" title="No. ' . count( $this->data ) . '">' . @$this->cur->temp . '&deg;C</div>' . "\n\t" . $tabs
+			. '<div class="s_cen">' . "\n\t\t" . $tabs
+			. ( $this->battery == 0 ? '<div class="batt0">&nbsp;</div>' . "\n\t\t" . $tabs : '' )
+			. ( isset( $this->min->humid ) ? '<div class="humid">' . "\n\t\t\t" . $tabs
+					. '<div class="h_max">' . $this->max->humid . '%</div>' . "\n\t\t\t" . $tabs
+					. '<div class="h_cur">' . $this->cur->humid . '%</div>' . "\n\t\t\t" . $tabs
+					. '<div class="h_min">' . $this->min->humid . '%</div>' . "\n\t\t\t" . $tabs 
+					. '</div>' . "\n\t\t" . $tabs : '' )
+			. '</div>' . "\n\t" . $tabs
+			. '<div class="t_max">' . @$this->max->temp . '&deg;C</div>' . "\n\t" . $tabs
+			. '<div class="t_min">' . @$this->min->temp . '&deg;C</div>' . "\n\t" . $tabs
+			. '</div>' . "\n";
 	}
 
 	function js_graph( $type = TEMPERATURE ) {
@@ -357,16 +390,21 @@ class Wind {
 if ( isset( $_REQUEST ) ) {
 	if ( isset( $_REQUEST['wind'] ) ) {
 		header( 'Content-Type: application/json' );
-		$wind = new Wind();
-		echo '{"wind":' . json_encode( $wind->fetch_current() ) . '}';
+		echo '{"wind":' . json_encode( Wind::fetch_current() ) . '}';
 	// 	echo '{"wind":{"id":"500","speed":"1.0","gust":"1.0","dir":"180","samples":"5","time":"2015-03-01 09:03:04","date":"1425196984"}}';
+		exit;
+	} else if ( isset( $_REQUEST['temp'] ) ) {
+		header( 'Content-Type: application/json' );
+		echo '{"sensor":';
+		echo json_encode( Sensor::fetch_all() );
+		echo '}';
 		exit;
 	}
 }
+
 $isMobile = preg_match("/(android|avantgo|blackberry|bolt|boost|cricket|docomo|fone|hiptop|mini|mobi|palm|phone|pie|tablet|up\.browser|up\.link|webos|wos)/i", $_SERVER["HTTP_USER_AGENT"]);
 
 header( 'Content-Type: text/html; charset=UTF-8' );
-
 ?>
 <!DOCTYPE html>
 
@@ -384,20 +422,29 @@ header( 'Content-Type: text/html; charset=UTF-8' );
 		body	{ font-family: Arial,Sans-serif; font-size: 12px; margin: 0px; padding: 0px; }
 		.r		{ text-align: right }
 		
-		div.graph  { display: inline-block; vertical-align: top; }
-		div.sens   { display: inline-block; }
-		div.s_name { font-size: 24px; text-align: center; }
+		div.team    { display: block; vertical-align: top; }
+		div.chart   { width: 350px; height: 150px; }
+		div.s_title { font-size: 24px; text-align: center; }
+		text.s_title { font-size: 24px; text-anchor: middle; }
+		
+		.c_head { display: inline-block; }
 		div.s_cen  { height: 28px; display: inline-block; float: left; }
 		
-		div.t_cur  { width: 48%; font-size: 24px; color: #666; float: left; display: block; text-align: right; padding-right: 10px}
+		div.t_cur  { width: 48%; font-size: 20px; color: #666; float: left; display: block; text-align: right; padding-right: 10px}
 		div.t_min, div.t_max { display: block; text-align: right; float:right; }
 		div.t_min  { color: #6060ff; }
 		div.t_max  { color: #ff6060; }
+		text.t_cur   { font-size: 20px; text-anchor: end; fill: #666; }
+		text.t_min   { font-size: 12px; text-anchor: end; fill: #6060ff; }
+		text.t_max   { font-size: 12px; text-anchor: end; fill: #ff6060; }
 		
 		div.humid  { font-size: 8px; text-align: center; background-color: #c6dcf3; }
 		div.h_min  { color: #6060ff; }
 		div.h_max  { color: #ff6060; }
 		div.h_cur  { color: #666; font-weight: bold; }
+		text.h_cur   { font-size: 8px; text-anchor: start; fill: #666; }
+		text.h_min   { font-size: 8px; text-anchor: start; fill: #6060ff; }
+		text.h_max   { font-size: 8px; text-anchor: start; fill: #ff6060; }
 
 		div.batt0, div.batt1, div.batt2, div.batt3 { background-image: url("bs16.png"); background-repeat: no-repeat; width: 16px; height: 16px; display: block;  }
 		div.batt3 { background-position:   0px 0px; }
@@ -406,6 +453,8 @@ header( 'Content-Type: text/html; charset=UTF-8' );
 		div.batt0 { background-position: -48px 0px; }
 
 		#wind { font-size: 24px; color: #2e8abb; text-align: center; width: 350px; height: 150px; }
+		.svg_wind { fill: #2e8abb; text-anchor: middle; }
+		
 	</style>
 	<script src="Chart.min.js"></script><!--http://www.chartjs.org/ ver 0.2-->
 	<script>
@@ -421,56 +470,97 @@ header( 'Content-Type: text/html; charset=UTF-8' );
 			}
 			return req;
 		}
+		function loadSensor( url ) {
+			var req = getHttpRequest();
+			req.onreadystatechange = function() {
+				if ( req.readyState == 4 ) {
+					var obj = JSON.parse( req.responseText );
+					if ( typeof obj.wind !== 'undefined' )
+						updateWind( obj.wind );
+					if ( typeof obj.sensor !== 'undefined' )
+						updateSensor( obj.sensor );
+				}
+			}
+			req.open( "GET", url, true );
+			req.send();
+		}
 		function updateWind( wind ) {
 			document.getElementById("svg_wind_speed").textContent = wind.speed + " m/s";
 			document.getElementById("svg_wind_dir").textContent   = wind.dir + "°";
 			document.getElementById("svg_wind_arrow").transform.baseVal.getItem(0).setRotate(wind.dir,25,65);
 		}
-		function loadWind() {
-			var req = getHttpRequest();
-			req.onreadystatechange = function() {
-				if ( req.readyState == 4 ) {
-					var obj = JSON.parse( req.responseText );
-					if ( obj.wind != 'undefined' )
-						updateWind( obj.wind );
+		function updateSensor( sensor ) {
+			var sens, head;
+			for ( sens in sensor ) {
+				head = document.getElementById("svg_sensor" + sens);
+				if ( head == null )
+					continue;
+				head.childNodes[3].style.visibility = ( sensor[sens].battery == 0 ? "visible" : "hidden" );
+				head.childNodes[5].textContent = sensor[sens].name;
+				if ( sensor[sens].type & <?= TEMPERATURE ?> ) {
+					head.childNodes[7].childNodes[1].textContent = sensor[sens].cur.temp + "°C";
+					head.childNodes[7].childNodes[3].textContent = sensor[sens].max.temp + "°C";
+					head.childNodes[7].childNodes[5].textContent = sensor[sens].min.temp + "°C";
+				}
+				if ( sensor[sens].type & <?= HUMIDITY ?> ) {
+					head.childNodes[9].childNodes[1].textContent = sensor[sens].max.humid + " %";
+					head.childNodes[9].childNodes[3].textContent = sensor[sens].cur.humid + " %";
+					head.childNodes[9].childNodes[5].textContent = sensor[sens].min.humid + " %";
 				}
 			}
-			req.open( "GET", "/weather/?wind=0", true );
-			req.send();
 		}
+		
 		window.onload = function() {
 <?php if ( !$isMobile ) { ?>
-			var tim = setInterval( function() {
-				loadWind();
-			}, 60000 );
+			var tim1 = setInterval( function() {
+				loadSensor( "/weather/?wind" );
+			}, 600000 );
+			var tim2 = setInterval( function() {
+				loadSensor( "/weather/?temp" );
+			}, 600000 );
 <?php } ?>
 		}
 	</script>
 </head>
 
 <body>
+	<svg id="defsSVG" class="chart" width="0" height="0" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" style="visibility: hidden; display: inline">
+		<defs>
+			<linearGradient id="windGradArrow" x1="0%" y1="0%"   x2="0%" y2="100%"><stop offset="0%" style="stop-color:#<?= SVG_COLOR_WIND_ARROW ?>; stop-opacity: 1" /><stop offset="100%" style="stop-color:#fff; stop-opacity: 1" /></linearGradient>
+			<linearGradient id="gradBg"    x1="0%" y1="100%" x2="0%" y2="0%"  ><stop offset="0%" style="stop-color:#<?= SVG_COLOR_BG ?>; stop-opacity: 1" /><stop offset="100%" style="stop-color:#fff; stop-opacity: 1" /></linearGradient>
+			<g id="svg_battery">
+				<polygon points="0,20 0,2 2.5,2 2.5,0 7.5,0 7.5,2 10,2 10,20 0,20" style="fill: white; stroke: black; stroke-width; 1px;" />
+				<polygon id="svg_battery_empty" points="2,18 2,15 8,13 8,18 2,18" style="fill: #b30000; stroke: none;">
+					<!--animate attributeType="CSS" attributeName="visibility" from="visible" to="hidden" dur="1s" repeatCount="indefinite" /-->
+				</polygon>
+			</g>
+			<g id="svg_head_bg">
+				<rect id="svg_wind_bg" x="0" y="0" width="100%" height="100%" rx="15" ry="15" style="fill:url(#gradBg); stroke: #2e8abb" />
+			</g>
+		</defs>
+	</svg>
 <?php
-	
+
 $groups  = new Groups( 1 );
 $groups->draw_graph();
 
 $wind = new Wind();
 if ( isset( $wind->current->speed ) ) {
-	$color_bg = '80a2e0'; $color_text = '2e8abb'; $color_arrow = '2e8abb';
 ?>
-	<div id="wind">
-		<div id="wind_title">Wind</div>
-		<svg id="windSVG" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-			<defs>
-				<linearGradient id="windGradArrow" x1="0%" y1="0%" x2="0%" y2="100%"><stop offset="0%" style="stop-color:#<?= $color_arrow ?>; stop-opacity: 1" /><stop offset="100%" style="stop-color:#fff; stop-opacity: 1" /></linearGradient>
-				<linearGradient id="windGradBg" x1="0%" y1="100%" x2="0%" y2="0%"><stop offset="0%" style="stop-color:#<?= $color_bg ?>; stop-opacity: 1" /><stop offset="100%" style="stop-color:#fff; stop-opacity: 1" /></linearGradient>
-			</defs>
-			<rect id="svg_wind_bg" width="350" height="150" style="fill:url(#windGradBg);" />
-			<g transform="translate(150,10)"><polygon id="svg_wind_arrow" points="25,0 0,50 20,45 5,130 25,125 45,130 30,45 50,50 25,0" style="fill: url(#windGradArrow); stroke: #<?= $color_arrow ?>; stroke-width; 5px; " transform="rotate(<?= $wind->current->dir ?> 25,65)" /></g>
-			<text id="svg_wind_speed" x="175" y="24" text-anchor="middle" style="fill:#<?= $color_text ?>"><?= $wind->current->speed ?> m/s</text>
-			<text  id="svg_wind_dir"x="175" y="48" text-anchor="middle" style="fill:#<?= $color_text ?>"><?= $wind->current->dir ?>&deg;</text>
+	<div id="wind" class="sens">
+		<div id="wind_title" class="s_title">Wind</div>
+		<svg id="windSVG" class="chart" width="350px" height="150px" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+			<use xlink:href="#svg_head_bg" />
+			<g transform="translate(150,10)"><polygon id="svg_wind_arrow" points="25,0 0,50 20,45 5,130 25,125 45,130 30,45 50,50 25,0" style="fill: url(#windGradArrow); stroke: #<?= SVG_COLOR_WIND_ARROW ?>; stroke-width; 5px; " transform="rotate(<?= $wind->current->dir ?> 25,65)" /></g>
+			<text id="svg_wind_speed" x="50%" y="24" class="svg_wind"><?= $wind->current->speed ?> m/s</text>
+			<text id="svg_wind_dir"   x="50%" y="48" class="svg_wind"><?= $wind->current->dir  ?>&deg;</text>
+<!-- 			<use x="10" y="10" xlink:href="#svg_battery" style="opacity: .75" /> -->
+		</svg>
+		<svg id="battSVG" class="chart" width="100px" height="150px" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
 		</svg>
 	</div>
-<?php } ?>
+<?php 
+} 
+?>
 </body>
 </html>
