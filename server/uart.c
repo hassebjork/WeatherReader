hassebjork/WeatherReader

commit 8b94ceb4b644c80b51e0d74f024a41a02f418cf7

Code cleanup, added comments and reformatted output

server/uart.c
@@ -33,10 +33,9 @@ extern ConfigSettings configFile;
 extern int pipeServer[2];
 extern int pipeParser[2];
 
-	// Timeout: http://stackoverflow.com/a/2918709/4405465
-	// http://www.unixwiz.net/techtips/termios-vmin-vtime.html
-	// http://stackoverflow.com/questions/6947413/how-to-open-read-and-write-from-serial-port-in-c
-	// http://www.tldp.org/HOWTO/Serial-Programming-HOWTO/x115.html
+/**
+ * UART-thread for receiving data from arduino connected by USB-serial cable
+ */
 void *uart_receive( void *ptr ) {
 	SerialDevice *sDev = (SerialDevice *) ptr;
 	
@@ -45,21 +44,24 @@ void *uart_receive( void *ptr ) {
 	char  buffer[256];
 	
 #if _DEBUG > 1
-	fprintf( stderr, "Uart thread %s:\x1B[30GEnabled\n", sDev->name );
+	fprintf( stderr, "Uart thread #%d %s:%*sEnabled\n", sDev->no, sDev->name, ( 14 - strlen( sDev->name ) ), "" );
 #endif
 	
+	// Open device
 	sDev->tty = open( sDev->name, O_RDONLY ); // O_RDWR | O_NOCTTY | O_NDELAY );
 	if ( sDev->tty == -1 ) {
 		perror( sDev->name );
 		return;
 	}
 	
+	// Get communication flags
 	if ( tcgetattr( sDev->tty, &options ) < 0 ) {
 		fprintf( stderr, "ERROR in uart_receive #%d: Configuring device\n",  sDev->no );
 		close( sDev->tty );
 		return;
 	}
 	
+	// Set communication flags
 	options.c_cflag = B115200 | CS8 | CREAD | CLOCAL; // 8 data bits, enable receiver, local line 
 	options.c_cflag &= ~PARENB | ~CSTOPB; // No parity, one stop bit 
 	options.c_iflag &= ~( IXON | IXOFF | IXANY ); // Disable software flow control
@@ -70,15 +72,16 @@ void *uart_receive( void *ptr ) {
 	tcflush( sDev->tty, TCIFLUSH ); 
 	tcsetattr( sDev->tty, TCSANOW, &options ); // Set tty with new settings immediately
 	
+	// Fetch id string from arduino. Reset and retry on fail.
 	uart_init( sDev );
-	// Retry initiating arduino
 	if ( sDev->type == UNDEFINED ) {
 		reset_arduino( sDev );
 		uart_init( sDev );
 	}
 	
-	printf( "#%d. Opened %s\x1B[30G\"%s\" found\n", sDev->no, sDev->name, SERIAL_TYPES[sDev->type] );
+	printf( "Opened #%d %s%*s\"%s\" found\n", sDev->no, sDev->name, ( 20 - strlen( sDev->name ) ), "", SERIAL_TYPES[sDev->type] );
 	
+	// Read serial port loop
 	while ( configFile.run && sDev->active ) {
 		rcount = read( sDev->tty, buffer, sizeof( buffer ) );
 		if ( rcount < 0 ) {
@@ -87,29 +90,33 @@ void *uart_receive( void *ptr ) {
 			fprintf( stderr, "ERROR in uart_receive #%d: Timeout\n", sDev->no, rcount );
 		} else if ( rcount > 4 ) {
 			buffer[rcount-1] = '\0';
-#if _DEBUG > 1
-			printf( "uart_receive (#%d):   \"%s\"\n", sDev->no, buffer );
-#endif
-			if ( configFile.is_client ) {
- 				if ( write( pipeServer[1], &buffer, rcount ) < 1 )
- 					fprintf( stderr, "ERROR in uart_receive #%d: pipeServer error\n", sDev->no );
-			} else {
- 				if ( write( pipeParser[1], &buffer, rcount ) < 1 )
+			
+			// Send data through client thread to remote server
+			if ( configFile.is_client )
+				if ( write( pipeServer[1], &buffer, rcount ) < 1 )
+					fprintf( stderr, "ERROR in uart_receive #%d: pipeServer error\n", sDev->no );
+			
+			// Send data to parser thread and database
+			else if ( write( pipeParser[1], &buffer, rcount ) < 1 )
  					fprintf( stderr, "ERROR in uart_receive #%d: pipeParser error\n", sDev->no );
-			}
+#if _DEBUG > 2
+			printf( "uart_receive #%d:%*s\"%s\" sent to %s\n", sDev->no, 14, "", buffer, ( configFile.is_client ? "client" : "parser" ) );
+#endif
 		}
-		// http://stackoverflow.com/questions/12777254/time-delay-in-c-usleep
-		usleep( 200 );
 	}
 	
-#if _DEBUG > 1
-	fprintf( stderr, "Uart thread %s:\x1B[30GClosing\n", sDev->name );
-#endif
 	close( sDev->tty );
+	free( sDev );
+#if _DEBUG > 1
+	fprintf( stderr, "Uart thread #%d %s:%*sClosing\n", sDev->no, sDev->name, 14 - strlen( sDev->name ), "" );
+#endif
 }
 
+/**
+ * Read first identifier string from arduino and select type
+ */
 void uart_init( SerialDevice *sDev ) {
-	sleep( 1 );
+	sleep( 1 );			// Wait for arduino to boot
 	char  buffer[256];
 	int   rcount = read( sDev->tty, buffer, sizeof( buffer ) );
 	if ( rcount >= 0 ) {
@@ -119,7 +126,7 @@ void uart_init( SerialDevice *sDev ) {
 		} else if ( strncmp( buffer, "[WS]", 4 ) == 0 ) {	// Wired-Sensor
 			sDev->active = 1;
 			sDev->type   = WIRE_SENSOR;
-		} else {
+		} else {											// Undefined - quit thread
 			sDev->active = 0;
 			sDev->type   = UNDEFINED;
 		}
@@ -137,9 +144,7 @@ void reset_arduino( SerialDevice *sDev ) {
 	sleep( 1 );
 	ioctl( sDev->tty, TIOCMBIS, &flag );
 #if _DEBUG > 1
-	char s[20];
-	printTime( s );
-	fprintf( stderr, "%s Resetting Arduino\n", s );
+	fprintf( stderr, "reset_arduino #%d %s%*sResetting Arduino\n", sDev->no, sDev->name, 13 - strlen( sDev->name ), "" );
 #endif
 }
 
@@ -153,6 +158,7 @@ char ** uart_get_serial( void ) {
 	char **temp, **array;
 	char   str[25], *dir_dev = "/dev/";
 	
+	// Allocate memory for string array
 	array    = malloc( sizeof( char *) );
 	if ( array ) {
 		array[0] = NULL;
@@ -161,6 +167,7 @@ char ** uart_get_serial( void ) {
 		return NULL;
 	}
 	
+	// Scan directory /dev
 	d = opendir( dir_dev );
 	if ( d && array ) {
 		while ( ( dir = readdir(d) ) != NULL ) {

server/config.c
@@ -62,7 +62,7 @@ int confReadFile( char *inFname, ConfigSettings *conf ) {
 		return( 1 );
 	}
 	
-	printf( "Configuration file:\x1B[30G%s\n", inFname );
+	printf( "Configuration file:%*s%s\n", 11, "", inFname );
 	
 	/* Read each line in the file and process the tags on that line. */
 	while ( fgets( rdBuf, READ_BUFSIZE, infd ) != NULL ) {

server/makefile
@@ -8,6 +8,7 @@ all: weather-reader
 
 weather-reader: weather-reader.c config.c parser.c sensor.c server.c uart.c
 	gcc -o ../bin/weather-reader \
+		-D_DEBUG=0 -g \
 		-pthread \
 		$(MYSQL_CFLAGS) \
 		$(MYSQL_LIBS) \
@@ -21,9 +22,9 @@ debug: weather-reader.c config.c parser.c sensor.c server.c uart.c test/test_ser
 		$(MYSQL_CFLAGS) \
 		$(MYSQL_LIBS) \
 		weather-reader.c config.c parser.c sensor.c server.c uart.c
-	ulimit -c unlimited && gcc -o ../bin/test-server \
-		-pthread -g \
-		test/test_server.c
+# 	ulimit -c unlimited && gcc -o ../bin/test-server \
+# 		-pthread -g \
+# 		test/test_server.c
 	@echo
 
 clean:

server/parser.c
@@ -42,13 +42,13 @@ void *parse_thread() {
 	int  result;
 
 #if _DEBUG > 1
-	fprintf( stderr, "Parser thread:\x1B[30GEnabled\n" );
+	fprintf( stderr, "Parser thread:%16sEnabled\n", "" );
 #endif
 	sensorInit();
 	
 	while ( ( result = read( pipeParser[0], &buffer, 254 ) ) > 0 && configFile.run ) {
-#if _DEBUG > 3
-		printf( "parse_thread: \tRecv \"%s\"\n", buffer );
+#if _DEBUG > 2
+		printf( "parse_thread:%17s\"%s\"\n", "", buffer );
 #endif
  		parse_input( buffer );
 	}

server/sensor.c
@@ -86,7 +86,7 @@ void sensorMysqlInit() {
 		mysql_close( mysql );
 		configFile.mysql = !configFile.mysql;
 	} else {
-		printf( "Using MySQL database:\x1B[30G\"mysql://%s/%s\"\n", configFile.mysqlServer, configFile.mysqlDatabase );
+		printf( "Using MySQL database:%*smysql://%s/%s\n", 9, "", configFile.mysqlServer, configFile.mysqlDatabase );
 		
 		/* Create database tables, if not exits */
 		for ( i=0; CREATE_TABLE_MYSQL[i] != 0; i++ ) {
@@ -580,35 +580,13 @@ time_t sensorTimeSync() {
 	if ( diff != 0 && update != 0 )
 		syncTime = syncTime / diff;
 	syncTime += 10;
-#if _DEBUG > 1
-	char s[20];
-	printTime( s );
-	fprintf( stderr, "%s SyncTime: %d\tCorr: %d\tDiff: %d\n", s, syncTime, correction, diff );
+	update = (time_t) ( now + syncTime - correction );
+#if _DEBUG > -1
+	struct tm *local = localtime( &now );
+	struct tm *upd   = localtime( &update );
+	fprintf( stderr, "[%02i:%02i:%02i] SyncTime:%*sCorrection: %+d sec\tNext update: %02i:%02i:%02i\n", 
+			local->tm_hour, local->tm_min, local->tm_sec, 10, "", -correction,
+			upd->tm_hour, upd->tm_min, upd->tm_sec );
 #endif
-	update = (time_t) now + syncTime - correction;
 	return (time_t) now - correction;
-}
-
-void sensorPrint( sensor *s ) {
-	if ( !s ) return;
-	printf( "id:%i Name:%s\tSensor:%X Protocol:%s Channel:%d "
-			"Rolling:%X Battery:%d Type:%d\n", 
-			s->rowid, s->name, s->sensor_id, s->protocol, s->channel, 
-			s->rolling, s->battery, s->type );
-}
-
-/**
- * printTime returns a formatted string of current time in s (min 20 characters)
- */
-void printTime( char *s ) {
-	struct tm *local;
-	time_t t = time(NULL);
-	local = localtime(&t);
-	sprintf(s, "[%i-%02i-%02i %02i:%02i:%02i]", 
-			(local->tm_year + 1900), 
-			(local->tm_mon) + 1, 
-			local->tm_mday, 
-			local->tm_hour,
-			local->tm_min, 
-			local->tm_sec );
 }

server/sensor.h
@@ -119,9 +119,6 @@ char sensorWind( sensor *s, float speed, float gust, int dir );
 char sensorSwitch( sensor *s, char value );
 DataWind *sensorWindInit();
 void sensorWindDataInit( sensor *s );
-
 time_t sensorTimeSync();
-void sensorPrint( sensor *s );
-void printTime( char *s );
 
 #endif

server/server.c
@@ -41,7 +41,7 @@ void *client_thread() {
 	char buffer[BUFF_SIZE];
 	int  result;
 	
-	fprintf( stderr, "Client enabled:\x1B[30GUsing server %s:%d\n", configFile.server, configFile.port );
+	fprintf( stderr, "Client enabled:%*sUsing server %s:%d\n", 15, "", configFile.server, configFile.port );
 	
 	while ( ( result = read( pipeServer[0], &buffer, BUFF_SIZE ) ) > 0 && configFile.run )
  		client_send( buffer );
@@ -51,7 +51,7 @@ void *client_thread() {
 		fprintf( stderr, "ERROR in client_thread: Pipe error %d\n", result );
 	
 #if _DEBUG > 1
-	fprintf( stderr, "Client thread:\x1B[30GClosing\n" );
+	fprintf( stderr, "Client thread:%*sClosing\n", 16, "" );
 #endif
 }
 
@@ -84,10 +84,10 @@ int client_send( char * buffer ) {
 		return 1;
 	}
 	
-#if _DEBUG > 1
-	printf( "client_send: \tSent \"%s\" %s\n", buffer, inet_ntoa(server.sin_addr) );
+#if _DEBUG > 2
+	printf( "client_send:%*s\"%s\" %s\n", 18, "", buffer, inet_ntoa(server.sin_addr) );
 #endif
-		
+	
 	close( sockServer );
 	return 0;
 }
@@ -98,7 +98,7 @@ void *server_thread() {
 	struct sockaddr_in server, client;
 	char buffer[BUFF_SIZE];
 	
-	fprintf( stderr, "Server enabled:\x1B[30GListening on port %d\n", configFile.port );
+	fprintf( stderr, "Server enabled:%*sListening on port %d\n", 15, "", configFile.port );
 		
 	// Create socket
 	sockServer = socket( AF_INET, SOCK_DGRAM, 0 );
@@ -124,13 +124,13 @@ void *server_thread() {
 		if ( rcount < 0 )
 			fprintf( stderr, "ERROR in server_thread: recvfrom failed!\n" );
 
-#if _DEBUG > 1
-		printf( "server_thread: \tRecv \"%s\" %s\n", buffer, inet_ntoa(client.sin_addr) );
+#if _DEBUG > 2
+		printf( "server_thread:%*s\"%s\" %s\n", 16, "", buffer, inet_ntoa(client.sin_addr) );
 #endif
 		if ( write( pipeParser[1], &buffer, rcount ) < 1 )
 			fprintf( stderr, "ERROR in server_thread: pipeParser error\n" );
 	}
 #if _DEBUG > 1
-	fprintf( stderr, "Server thread:\x1B[30GClosing\n" );
+	fprintf( stderr, "Server thread:%*sClosing\n", 16, "" );
 #endif
 }

server/weather-reader.c
@@ -23,7 +23,7 @@ int main( int argc, char *argv[]) {
 	}
 	
 #if _DEBUG > 0
-	fprintf( stderr, "Debug info:\x1B[30GEnabled\n" );
+	fprintf( stderr, "Debug info:%*sEnabled\n", 19, "" );
 #endif
 	
 	// Start client to send data to remote server
@@ -47,11 +47,13 @@ int main( int argc, char *argv[]) {
 		}
 	}
 	
+	// Scan for USB-serial devices
 	serName = uart_get_serial();
 	for ( SerDevNum = 0; serName[SerDevNum]; SerDevNum++ );
 	
+	// Start a thread for each USB-serial device
 	sDev = malloc( sizeof( SerialDevice ) * SerDevNum );
-	pthread_t    threadUart[SerDevNum];
+	pthread_t threadUart[SerDevNum];
 	for ( i = 0; serName[i]; i++ ) {
 		sDev[i].name   = strdup( serName[i] );
 		sDev[i].active = 0;
@@ -77,7 +79,6 @@ int main( int argc, char *argv[]) {
 	signal( SIGINT, signal_interrupt );				// Program exit
 	
 	// Wait for threads to complete
-// 	pthread_join( threadUart, NULL);
 	if ( configFile.is_client ) {
 		pthread_join( threadServer, NULL);
 	} else if ( configFile.is_server ) {

commit 338b7d708a0eb8ead4ced93d4dc14c756f8d7307

Fixed bug introduced in 8b94ceb4b644

server/uart.c
@@ -92,16 +92,21 @@ void *uart_receive( void *ptr ) {
 			buffer[rcount-1] = '\0';
 			
 			// Send data through client thread to remote server
-			if ( configFile.is_client )
+			if ( configFile.is_client ) {
 				if ( write( pipeServer[1], &buffer, rcount ) < 1 )
 					fprintf( stderr, "ERROR in uart_receive #%d: pipeServer error\n", sDev->no );
+#if _DEBUG > 1
+				fprintf( stderr, "uart_receive #%d:%*s\"%s\" sent %d bytes to parser\n", sDev->no, 14, "", buffer, rcount );
+#endif
 			
 			// Send data to parser thread and database
-			else if ( write( pipeParser[1], &buffer, rcount ) < 1 )
-				fprintf( stderr, "ERROR in uart_receive #%d: pipeParser error\n", sDev->no );
-#if _DEBUG > 2
-			printf( "uart_receive #%d:%*s\"%s\" sent to %s\n", sDev->no, 14, "", buffer, ( configFile.is_client ? "client" : "parser" ) );
+			} else {
+				if ( write( pipeParser[1], &buffer, rcount ) < 1 )
+					fprintf( stderr, "ERROR in uart_receive #%d: pipeParser error\n", sDev->no );
+#if _DEBUG > 1
+				fprintf( stderr, "uart_receive #%d:%*s\"%s\" sent %d bytes to client\n", sDev->no, 4, "", buffer, rcount );
 #endif
+			}
 		}
 	}
 	